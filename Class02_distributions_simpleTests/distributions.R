8ccfcb3132513c711e4902253378f66067ff0ede
Alan Bergland <aob2x@virginia.edu>
2026-01-13 14:54:24 -0500

    updates

--- Class02_distributions_simpleTests/distributions.R
+++ Class02_distributions_simpleTests/distributions.R
@@ -15,7 +15,7 @@
 ### our toy-dataset
   ShrinkingSeals
   
-  ### your turn: what are the dimensions and structure of this object?
+### your turn: what are the dimensions and structure of this object?
     
 ### plotting the data. What are some noticable features of the data?
   ggplot(data=ShrinkingSeals, aes(length)) + geom_histogram()
@@ -31,21 +31,9 @@
   sd(ShrinkingSeals$length)/sqrt(length(ShrinkingSeals$length)) ### the standard error
 
 ### your turn: can you transform age in days into a new column of age in years, to match the publication?
-  ShrinkingSeals <- as.data.table(ShrinkingSeals)
-  ShrinkingSeals[,age_years:=age/365]
-  ShrinkingSeals[,age_years_integer:=floor(age/365 - 0.5)]
-  
-  ggplot(data=ShrinkingSeals, aes(y=length, x=age, color=as.factor(age_years_integer))) + geom_point()
 
 ### your turn: calculate the mean, sd, variance, sample size, standard errors for each unique value of age, in years  
-  ss.ag <- ShrinkingSeals[,list(mean=mean(length), 
-                                sd=sd(length), 
-                                var=var(length), 
-                                se=sd(length)/sqrt(length(age)),
-                                nSeals=length(age),
-                                .N),
-                          list(years=age_years_integer)]
-
+ 
 ### our turn. Let's make a plot. What do we notice?
   se.plot <- ggplot(data=ss.ag, aes(y=mean, x=years)) + 
     geom_line(color="black") +
@@ -67,61 +55,18 @@
   
   se.plot + sd.plot + plot_annotation(tag_levels="A")
   
-### is there a statistically significant difference in the length between years 2 & 3?
+### is there a statistically significant difference in the length between years 2 & 3? Let's use a t-test
   setkey(ShrinkingSeals, age_years_integer)
-  ShrinkingSeals[J(c(2,3))]
+  ?t.test
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-### how well do we estimate the moments at different sample sizes?
-### we can make a random subsample using the `sample` function
-sample(ShrinkingSeals$length, 20, replace=F)
+### your turn: what two parameters do we need for the t-test, and how to we extract them from the data?
+   
+### 
 
-### we can take the mean of a sub-sample
-mean(sample(ShrinkingSeals$length, 20, replace=F))
-
-### if we want to understand the full dynamics of how the moments change, we will need to perform many resamplings at different sample sizes
-sampleSizes <- seq(from=2, to=dim(ShrinkingSeals)[1], by=10)
-
-nSamples <- 20
-
-moments.dt <- foreach(n=sampleSizes, .combine="rbind")%do%{
-  foreach(samp=1:nSamples, .combine="rbind")%do%{
-    tmp <- sample(ShrinkingSeals, n, replace=F)
-    data.table(n=n, samp=samp, mean=mean(tmp$length), sd=sd(tmp$length), se=sd(tmp$length)/sqrt(n))
-  }
-}
-
-ggplot(data=moments.dt, aes(y=mean, x=n)) + geom_point()
-ggplot(data=moments.dt, aes(y=sd, x=n)) + geom_point()
-ggplot(data=moments.dt, aes(y=se, x=n)) + geom_point()
-
-### putting it all together
-mean_plot <- ggplot(data=moments.dt, aes(y=mean, x=n)) + geom_point() + ggtitle("Mean")
-sd_plot <- ggplot(data=moments.dt, aes(y=sd, x=n)) + geom_point() + ggtitle("SD")
-se_plot <- ggplot(data=moments.dt, aes(y=se, x=n)) + geom_point() + ggtitle("SE")
-
-mean_plot + sd_plot + se_plot + plot_annotation(tag_levels="A")
-
-
-
-
-
-
-
+  
+  
+  
+  
+  
+  
+  
