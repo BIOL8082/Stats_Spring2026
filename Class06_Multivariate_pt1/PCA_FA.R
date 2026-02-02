3156d8b7e50c0b1769e03be53678160e7e6407f4
Alan Bergland <aob2x@virginia.edu>
2026-02-02 18:29:42 -0500

    markers

--- Class06_Multivariate_pt1/PCA_FA.R
+++ Class06_Multivariate_pt1/PCA_FA.R
@@ -31,7 +31,8 @@ install.packages("foreach")
     
     setkey(pheno.l.mp, ral_id)
     pheno.trim <- pheno.l.mp[J(missing.dgrp[missing<.2]$ral_id)]
-  
+    pheno.trim
+    
   ### pad missing
     pheno.trim.ag <- pheno.trim[,list(mu=mean(value, na.rm=T)), list(variable)]
     pheno.trim <- merge(pheno.trim, pheno.trim.ag, by="variable")
@@ -42,12 +43,12 @@ install.packages("foreach")
   ### long back to wide
     pheno.use <- dcast(pheno.trim, ral_id~variable, measure.var="pad_value_pad")
     dim(pheno.use)  
- 
+    summary(pheno.use)
 
 ### cormat on observed data
     cor.mat <- cor(mat)
     cor.dt <- as.data.table(reshape2::melt(cor.mat))
-    cor.plot <-ggplot(data=cor.dt, aes(x=Var1, y=Var2, fill=value)) + geom_tile() + theme(axis.text = element_blank()) + scale_fill_viridis()
+    cor.plot <- ggplot(data=cor.dt, aes(x=Var1, y=Var2, fill=value)) + geom_tile() + theme(axis.text = element_blank()) + scale_fill_viridis()
     cor.plot
     
 
@@ -55,13 +56,14 @@ install.packages("foreach")
     mat <- as.matrix(pheno.use[,-"ral_id"])
     rownames(mat) <- pheno.use$ral_id
     pheno.pca <- prcomp(mat, center=T, scale=T)
+    summary(pheno.pca)
     str(pheno.pca)    
   
 ### Scree plot
     scree.dt <- data.table(PC=c(1:95), var=pheno.pca$sdev^2)
     scree.dt[,percent_variance:=var/sum(scree.dt$var)]
     
-    scree.plot <- ggplot(data=scree.dt, aes(x=PC, y=var)) + geom_point(color="black", size=3) +
+    scree.plot <- ggplot(data=scree.dt, aes(x=PC, y=percent_variance)) + geom_point(color="black", size=3) +
       theme_bw() +
       ylab("Variance")
     
@@ -79,7 +81,9 @@ install.packages("foreach")
     loadings.plot
     
 ### Your turn: which phenotypes are driving the strong positive loadings onto PC1? What about PC2?
-  
+    loadings.pca[,c("trait", "PC1")][PC1 < -.2]
+    
+    
 ### Projections
     str(pheno.pca)
     proj <- as.data.table(pheno.pca$x)
@@ -89,7 +93,12 @@ install.packages("foreach")
     pc3_pc4 <- ggplot(data=proj, aes(x=PC3, y=PC4)) + geom_point() + theme_bw()
     
 ### your turn: are the phenotypes that are driving the strong positive loading on to PC1 highly correlated?    
+  summary(cor.dt[grep("DDT", Var1)][grep("DDT", Var2)]$value)
   
+  
+  & grep("DDT", Var2)]
+    
+    
 ### PCA megaplot
     layout <- c("
       ABEE
@@ -99,13 +108,16 @@ install.packages("foreach")
     scree.plot + loadings.plot + pc1_pc2 + pc3_pc4 + cor.plot + plot_layout(design=layout)
     
 ### factor analysis: https://library.virginia.edu/data/articles/getting-started-with-factor-analysis
-   dgrp.fa <- fa(mat, nfactor=48, rotate="varimax", fm="ml", n.obs=141)
+   dgrp.fa <- fa(mat, nfactor=50, rotate="varimax", fm="ml", n.obs=141)
    summary(dgrp.fa)     
    str(dgrp.fa)
    dgrp.scores <- as.data.table(dgrp.fa$scores)
    dgrp.scores[,ral_id:=as.numeric(rownames(dgrp.fa$scores))]
    
 ### inversions
+    setnames(inv, c("DGRP Line", "In(2L)t"), c("DGRP", "In2Lt"))
+    inv[,ral_id:=as.numeric(tstrsplit(DGRP, "_")[[2]])]
+    
     proj <- merge(proj, inv[,c("ral_id", "In2Lt")], by="ral_id")
     proj[,In2Lt:=factor(In2Lt, levels=c("ST", "INV/ST", "INV"))]
     summary(aov(PC1~In2Lt, proj))
